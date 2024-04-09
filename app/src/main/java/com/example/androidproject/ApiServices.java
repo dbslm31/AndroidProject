a8c72f80ca4a429229edec4a3122f64111bee441
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2024-04-09 16:00:16 +0200

    ApiService VoteOnCatImahe

--- app/src/main/java/com/example/androidproject/ApiServices.java
+++ app/src/main/java/com/example/androidproject/ApiServices.java
@@ -5,11 +5,13 @@ import android.graphics.Bitmap;
 import android.util.Log;
 import android.widget.ImageView;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -20,14 +22,17 @@ import org.json.JSONObject;
 
 import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
-    public class ApiServices {
+public class ApiServices {
 
         private static final String URL_API_SEARCH_IMAGE = "https://api.thecatapi.com/v1/images/search";
         private static final String URL_END_BREEDS = "?has_breeds=1";
         private static final String API_KEY = "&api_key=live_Vd2XNWnMBpWTsjoBUOyEmmJ0xVCGtxQsVgdaW6FPD6cputraAjpNw9tTqkcFmtlF";
+        private static final String URL_API_VOTE = "https://api.thecatapi.com/v1/votes";
 
         public void getRandomCatImage(Context context, Response.ErrorListener errorListener) {
             RequestQueue queue = Volley.newRequestQueue(context);
@@ -40,10 +45,14 @@ import java.util.List;
                             for (int i = 0; i < jsonArray.length(); i++) {
                                 JSONObject object = jsonArray.getJSONObject(i);
 
+                                //Set Cat ImageId
+                                String catImageId = object.getString("id");
+                                Cat cat = new Cat();
+                                cat.setImageId(object.getString("id"));
+
                                 //Set Cat Images
                                 String imageUrl = object.getString("url");
                                 Log.d("Cat Image URL", imageUrl);
-                                Cat cat = new Cat();
                                 cat.setUrl(object.getString("url"));
 
                                 //Set Cat Infos
@@ -52,11 +61,11 @@ import java.util.List;
                                     cat.setName(breedsInfo.optString("name", ""));
                                     cat.setOrigin(breedsInfo.optString("origin", ""));
                                     cat.setAdaptability(breedsInfo.optInt("adaptability", 0));
-                                    cat.setAffection_level(breedsInfo.optInt("affection_level", 0));
-                                    cat.setChild_friendly(breedsInfo.optInt("child_friendly", 0));
-                                    cat.setEnergy_level(breedsInfo.optInt("energy_level", 0));
+                                    cat.setAffectionLevel(breedsInfo.optInt("affection_level", 0));
+                                    cat.setChildFriendly(breedsInfo.optInt("child_friendly", 0));
+                                    cat.setEnergyLevel(breedsInfo.optInt("energy_level", 0));
                                     cat.setIntelligence(breedsInfo.optInt("intelligence", 0));
-                                    cat.setSocial_needs(breedsInfo.optInt("social_needs", 0));
+                                    cat.setSocialNeeds(breedsInfo.optInt("social_needs", 0));
                                 }
                             }
                         } catch (JSONException e) {
@@ -65,5 +74,38 @@ import java.util.List;
                     }, errorListener);
 
             queue.add(request);
+        }
+
+        public void voteOnCatImage(Context context, String imageId, int voteValue, Response.ErrorListener errorListener) {
+            RequestQueue queue = Volley.newRequestQueue(context);
+            String url = URL_API_VOTE;
+            JSONObject voteData = new JSONObject();
+            try {
+
+                /*
+                ** Note pour Nils —
+                ** OnClick sur le button
+                ** voteValue doit être 1 si cest un like
+                ** et -1 si c'est un dislike
+                */
+
+                voteData.put("image_id", imageId);
+                voteData.put("value", voteValue);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, voteData,
+                    response -> Log.d("Vote", "Vote successful"),
+                    errorListener) {
+                @Override
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    HashMap<String, String> headers = new HashMap<>();
+                    headers.put("Content-Type", "application/json");
+                    headers.put("x-api-key", API_KEY);
+                    return headers;
+                }
+            };
+            queue.add(jsonObjectRequest);
         }
     }
