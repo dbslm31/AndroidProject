efb01a05ecc5f332c421a23310788d9980e87753
dbslm31 <104209863+dbslm31@users.noreply.github.com>
2024-04-09 14:28:43 +0200

    FEAT / Created Cat Model

---
+++ app/src/main/java/com/example/androidproject/ApiServices.java
@@ -0,0 +1,33 @@
+package com.example.androidproject;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ApiServices {
+
+    private static String URL_API_SEARCH_IMAGE=" https://api.thecatapi.com/v1/images/search";
+
+
+
+
+}
